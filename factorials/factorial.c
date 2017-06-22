e7455a7900180560ac95e6bc6a7d0c3fd73a2669
Jesse <ericksenjesse@gmail.com>
2017-06-22 16:47:07 -0700

    factorials finished

--- factorials/factorial.c
+++ factorials/factorial.c
@@ -1,27 +1,21 @@
 #include <stdio.h>
 
-int factorial(int number){
+long factorial(int number){
 
-    int result = 0;
-    if( number = 0){
-       return 1;
-    }
+    int c;
+    long factorial = 1;
 
-    else{
-        int currentNum = 1;
-        for(currentNum = 1; currentNum <= number; currentNum++){
-            result += (currentNum*number);
-            currentNum++;
-        }
-        return result;
+    for( c = 1; c <= number; c++){
+       factorial = factorial*c;
     }
+    return factorial;
 
 }
 
 int main() {
   int i;
   for(i = 0; i <= 20; i++){
-     printf("Number: %d \t Factorial: %d \n", i, (factorial(i)));
+     printf("Number: %d \t Factorial: %ld \n", i, (factorial(i)));
   }
 
   return 0;
