4084efa7b19d242741d655a8960e415a75caf481
admin <admin@localhost.localdomain>
2017-06-22 20:02:18 -0700

    assemblyu

---
+++ Assemblyu/Memory Layout of C Programs - GeeksforGeeks_files/a_data_002/ads_data/ads_data/5474a5f9e4b05cf9e13a804f_si.js
@@ -0,0 +1,273 @@
+(function () {
+    _(
+        {"status":{"code":"OK"},"jsVersion":"17.16.1039","id":"57f56e7fe3bb564182746711","wlcid":"5474a5f9e4b05cf9e13a804f","bid":{"id":"58c7cae0f78ced08a033fda2","videos":[{"videoId":"5910b5628c08e0401f055bef","name":"BITSNGO: London Travel Guide","videoUrls":["https://videocdn.bitsngo.net/videos/London-300x168.mp4"],"thumbnailId":"5910bc3e5ab535759fd4cd41","thumbnail":"https://cdn-ssl.vidible.tv/prod/2017-05/08/5910b5628c08e0401f055bef/5910bc3e5ab535759fd4cd41_60x60_U_v1.jpg","fullsizeThumbnail":"https://cdn-ssl.vidible.tv/prod/2017-05/08/5910b5628c08e0401f055bef/5910bc3e5ab535759fd4cd41_o_U_v1.jpg","subtitles":[],"captions":{},"metadata":{"duration":112570,"location":"Lonon, United Kingdom","keywords":["London","Travel","Tourism","Sightseeing"],"videoType":"VOD","commonRating":{"value":"G","descriptors":[],"minAge":0}},"videoSourceType":"FILE","iabCategories":[{"id":"/AOL23","name":"/Travel"}],"studioName":"T2V Inc","cuePoints":[],"cs":{"priority":false}}]},"playerTemplate":{"initialization":"autoplay","mobileInit":"CLICK","mobileAutoplayWifiOnly":false,"fullscreenOnUnmute":false,"previewPoster":"IMAGE","sound":"muted","initialVolume":1.0,"videosToPlay":10,"videosToRequest":1,"shuffleVideos":false,"prerollFrequency":0,"backgroundSkinLocation":{"x":0,"y":0,"w":0,"h":0},"controlsSkin":"https://cdn-ssl.vidible.tv/prod/player/swf/17.06.342/ControlsAOL_5.swf","controlsSkinLocation":{"x":0,"y":213,"w":300,"h":0},"videoLocation":{"x":0,"y":0,"w":300,"h":250},"afterVideosUI":"next","scrubBarSkin":"https://cdn-ssl.vidible.tv/prod/2013-03/10/511e8160e4b0bf40bd0340a7_v2.swf","coveringsSkin":"https://cdn-ssl.vidible.tv/prod/player/swf/17.06.342/Coverings.swf","coveringsSkinLocation":{"x":0,"y":0,"w":300,"h":250},"surroundSkinLocation":{"x":0,"y":0,"w":0,"h":0},"playerWidth":300,"playerHeight":250,"controlsAutoPosition":true,"controlsChromeless":true,"controlsBackgroundAlpha":"1.0","controlsConfig":{"colorSchema":{"mainColor":{"backgroundColor":16777215,"backgroundAlpha":1.0}}},"nielsenSiteCampaign":"nlsn31325","nielsenChannelCampaign":"cmp185270","backgroundFill":true,"backgroundColor":0,"surround3DBevelShadowColor":16777215,"surround3DBevelHighlightColor":16777215,"surroundInnerRadius":0,"surroundCornerRadius":0,"surroundHole":false,"surround3D":false,"surround3DBevelSize":0,"surround3DBevelStrength":0.0,"extras":[{"extraId":"52a827a5e4b019442ceb808e","uiPosition":"DEFAULT","region":{"x":0,"y":0,"w":0,"h":0},"blocking":true,"urls":{"FLASH":"https://cdn-ssl.vidible.tv/prod/player/swf/17.06.342/click-throughs.swf","JS":"https://cdn-ssl.vidible.tv/prod/player/js/17.16.1039/extras/clickthrough.js"},"subType":"COMMON"},{"extraId":"54e85e5ce4b0083d0ce32f03","config":{"Google":{"enabled":"false"},"Weibo":{"enabled":"false"},"Like":{"enabled":"true"},"Email":{"enabled":"false"},"showOnAd":"false","Embed":{"enabled":"false"},"sharePosition":{"shareAlignment":"right","shareTopMargin":0},"Twitter":{"enabled":"true"},"Facebook":{"enabled":"true"}},"uiPosition":"DEFAULT","region":{"x":0,"y":0,"w":0,"h":0},"blocking":true,"urls":{"FLASH":"https://cdn-ssl.vidible.tv/prod/player/swf/17.06.342/share-tuner.swf","JS":"https://cdn-ssl.vidible.tv/prod/player/js/17.16.1039/extras/share.js"},"subType":"COMMON"}],"publisherPayout":"None","publisherAmount":0.0,"metaData":{},"showLogo":false,"autoplayInView":0,"vrRenderer":"O2","isResponsive":false,"csid":"54ef07a8e4b020e2f828c02c","HLSExtra":"https://cdn-ssl.vidible.tv/prod/player/swf/17.06.342/hls-plugin.swf","IMAExtra":"https://cdn-ssl.vidible.tv/prod/player/swf/17.06.342/ima-ad-module.swf","YuMeExtra":"https://cdn-ssl.vidible.tv/prod/player/swf/17.06.342/yume-ad-module.swf","FreeWheelExtra":"https://cdn-ssl.vidible.tv/prod/player/swf/17.06.342/free-wheel-module.swf","VASTExtra":"https://cdn-ssl.vidible.tv/prod/player/swf/17.06.342/vast-ad-engine.swf","PlayerAdSystem":"https://cdn-ssl.vidible.tv/prod/player/swf/17.06.342/vidible-ad-server.swf","UIExtra":"https://cdn-ssl.vidible.tv/prod/player/swf/17.06.342/controls-sticky.swf","AgeGateExtra":"https://cdn-ssl.vidible.tv/prod/player/swf/17.06.342/age-gate.swf","SubtitlesExtra":"https://cdn-ssl.vidible.tv/prod/player/swf/17.06.342/subtitles.swf","ClickExtra":"https://cdn-ssl.vidible.tv/prod/player/swf/17.06.342/click-throughs.swf"},"timestamp":"20170623030057662","adSettings":{"podSize":8,"prerollInterleave":0,"midrollTiming":"0%;5%;10%;20%;30%;40%;50%;60%;70%;80%","maxAdsCount":22,"strictSiteCheckForAds":false,"blockContentOnAdBlocker":false,"domainOptimisation":true,"adStrategy":"ADSET_BASED","useSsai":false,"companions":[],"aeg":[],"asids":["59140418955a3135a9c7fd77"]},"playerWidget":{"playerType":"HTML5","url":"https://cdn-ssl.vidible.tv/prod/player/swf/17.06.342/player-vast.swf","adOnly":false,"isAol":false},"geo":{"country":"usa","region":"wa","zipCode":"98248","areaCode":"360","connSpeed":"cable"},"brandedContent":false,"sid":"19f75588-1198-4486-975c-8c08b6c94842","bsf":{"bs":{"l":"GREEN","t1":["Technology & Computing"],"rtb":["IAB19"],"r":2140},"f":{"o2":0.0}}}
+    );
+    function _(json) {
+                    /*
+     Developed by Robert Nyman, http://www.robertnyman.com
+     Code/licensing: http://code.google.com/p/getelementsbyclassname/
+     */
+            var getElementsByClassName = function (className, tag, elm) {
+                if (document.getElementsByClassName) {
+                    getElementsByClassName = function (className, tag, elm) {
+                        elm = elm || document;
+                        var elements = elm.getElementsByClassName(className),
+                            nodeName = (tag) ? new RegExp("\\b" + tag + "\\b", "i") : null,
+                            returnElements = [],
+                            current;
+                        for (var i = 0, il = elements.length; i < il; i += 1) {
+                            current = elements[i];
+                            if (!nodeName || nodeName.test(current.nodeName)) {
+                                returnElements.push(current);
+                            }
+                        }
+                        return returnElements;
+                    };
+                }
+                else if (document.evaluate) {
+                    getElementsByClassName = function (className, tag, elm) {
+                        tag = tag || "*";
+                        elm = elm || document;
+                        var classes = className.split(" "),
+                            classesToCheck = "",
+                            xhtmlNamespace = "http://www.w3.org/1999/xhtml",
+                            namespaceResolver = (document.documentElement.namespaceURI === xhtmlNamespace) ? xhtmlNamespace : null,
+                            returnElements = [],
+                            elements,
+                            node;
+                        for (var j = 0, jl = classes.length; j < jl; j += 1) {
+                            classesToCheck += "[contains(concat(' ', @class, ' '), ' " + classes[j] + " ')]";
+                        }
+                        try {
+                            elements = document.evaluate(".//" + tag + classesToCheck, elm, namespaceResolver, 0, null);
+                        }
+                        catch (e) {
+                            elements = document.evaluate(".//" + tag + classesToCheck, elm, null, 0, null);
+                        }
+                        while ((node = elements.iterateNext())) {
+                            returnElements.push(node);
+                        }
+                        return returnElements;
+                    };
+                }
+                else {
+                    getElementsByClassName = function (className, tag, elm) {
+                        tag = tag || "*";
+                        elm = elm || document;
+                        var classes = className.split(" "),
+                            classesToCheck = [],
+                            elements = (tag === "*" && elm.all) ? elm.all : elm.getElementsByTagName(tag),
+                            current,
+                            returnElements = [],
+                            match;
+                        for (var k = 0, kl = classes.length; k < kl; k += 1) {
+                            classesToCheck.push(new RegExp("(^|\\s)" + classes[k] + "(\\s|$)"));
+                        }
+                        for (var l = 0, ll = elements.length; l < ll; l += 1) {
+                            current = elements[l];
+                            match = false;
+                            for (var m = 0, ml = classesToCheck.length; m < ml; m += 1) {
+                                match = classesToCheck[m].test(current.className);
+                                if (!match) {
+                                    break;
+                                }
+                            }
+                            if (match) {
+                                returnElements.push(current);
+                            }
+                        }
+                        return returnElements;
+                    };
+                }
+                return getElementsByClassName(className, tag, elm);
+            };
+            var cl = "vdb_57f56e7fe3bb5641827467115474a5f9e4b05cf9e13a804f";
+            var containers = getElementsByClassName(cl, null, document.body);
+            var idx = containers.length;
+            var c = containers[0];
+            if (document.currentScript) {
+                while (--idx > 0) {
+                    if (document.currentScript.parentNode == containers[idx]) {
+                        c = containers[idx];
+                        break;
+                    }
+                }
+            }
+            c.className = c.className.replace(new RegExp("(\\s*)" + cl + "\\s*", "g"), "$1");
+                var utilsObj = {
+    GUID: function () {
+        var ts = 20170623030057662 || 0;
+        if (typeof ts !== 'number') {
+            ts = parseInt(ts, 10) || 0;
+        }
+
+        var d = utilsObj.numberizeUrl() + new Date().getTime() + ts;
+        if (window.performance && typeof window.performance.now === "function") {
+            d += performance.now();
+        }
+        var uuid = 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function (c) {
+            var r = (d + Math.random() * 16) % 16 | 0;
+            d = Math.floor(d / 16);
+            return (c == 'x' ? r : (r & 0x3 | 0x8)).toString(16);
+        });
+        return uuid;
+    },
+
+    numberizeUrl: function () {
+        var url = utilsObj.getPageUrl() || utilsObj.getDocumentReferrer();
+        return utilsObj.numberizeString(url);
+    },
+
+    numberizeString: function (str) {
+        var strNumber = 0;
+        if (typeof str === "string") {
+            for (var i = 0, len = str.length; i < len; i++) {
+                strNumber += str.charCodeAt(i);
+            }
+        }
+        return strNumber;
+    },
+
+    getPageUrl: function () {
+        try {
+            return window.top.location.href;
+        }
+        catch (e) {
+            return null;
+        }
+    },
+
+    getDocumentReferrer: function () {
+        try {
+            return window.top.document.referrer;
+        }
+        catch (e) {
+            return null;
+        }
+    },
+
+    getTopMostWindow: function (currWindow) {
+        var currP = currWindow || window;
+        while (currP) {
+            try {
+                var newP = currP.parent;
+                // try to reach the document, if didn't succeed - goes to catch {}
+                var doc = newP.document;
+                if (!doc) break;
+                // reached the top
+                // newP is ok. use it.
+                currP = newP;
+                if (newP == newP.parent) break;
+
+            } catch (ex) {
+                break;
+            }
+        }
+        return currP;
+    },
+
+    getTopMostLocation: function (win) {
+        var top = this.getTopMostWindow(win);
+        if (top == window.top) {
+            return top.location.href;
+        } else {
+            return top.document && top.document.referrer || top.location.href;
+        }
+    }
+
+};
+        var vdb_params = c.getAttribute("vdb_params");
+        var macro_params = "";
+        if (vdb_params && macro_params){
+            vdb_params = vdb_params.concat("&");
+        }
+        var p = (vdb_params || "") + macro_params;
+
+        var cb = /(?:[\?&]|^)m\.cb=(.*?)(&m\..*?=|$)/g.exec(p);
+        cb = cb && cb[1] || Math.random();
+        var ur = /(?:[\?&]|^)m\.url=(.*?)(&m\..*?=|$)/g.exec(p);
+        ur = ur && ur[1];
+        function ee(pn, v, dv) {
+            var ve = dv && dv(v) || v;
+            if (ve == v) {
+                try {
+                    ve = decodeURIComponent(v);
+                    ve = encodeURIComponent(ve);
+                } catch (e) {
+                    ve = encodeURIComponent(v);
+                }
+            }
+            p = p.replace("m." + pn + "=" + v, "m." + pn + "=" + ve);
+            v = ve;
+            return v;
+        }
+
+        ee("url", ur);
+        p += p && "&";
+        var ifr = window != top;
+        var r = encodeURIComponent(utilsObj.getTopMostLocation());
+        var i = document.createElement("img");
+        var it = new Date().getTime();
+        var pt = "html5";
+        var sid = "19f75588-1198-4486-975c-8c08b6c94842";
+        
+        var scriptParams = "ifr=" + ifr + "&pt=" + pt + "&sid=" + sid + "&" + p + "r=" + r;
+        var pixelParams = scriptParams + "&cb=" + cb;
+                                    var si = c.getElementsByTagName('img');
+                var srcSubstr = 'https://trk.vidible.tv/trk/impression.gif';
+                var isImpressionExist = false;
+                for (var ik = 0; ik < si.length; ik++) {
+                    if (si[ik].src.indexOf(srcSubstr) !== -1) {
+                        isImpressionExist = true;
+                        break;
+                    }
+                }
+                if (!isImpressionExist) {
+                    i.src = "https://trk.vidible.tv/trk/impression.gif?pid=57f56e7fe3bb564182746711&bcid=5474a5f9e4b05cf9e13a804f&s=true&" + pixelParams + "&recover=true&pv=17.16.1039";
+                    i = document.createElement("img");
+                    var et = encodeURIComponent('player error');
+                    var st = encodeURIComponent('workflow error');
+                    var dt = encodeURIComponent('Static impression was removed');
+                    i.src = "https://trk.vidible.tv/trk/error.gif?pid=57f56e7fe3bb564182746711&bcid=5474a5f9e4b05cf9e13a804f&s=true&" + pixelParams + "&et=" + et + "&st=" + st + "&dt=" + dt;
+                    i = document.createElement("img");
+                }
+                        i.src = "https://trk.vidible.tv/trk/js-loaded.gif?pid=57f56e7fe3bb564182746711&bcid=5474a5f9e4b05cf9e13a804f&s=true&" + pixelParams;
+                var s = document.createElement("script");
+        s.type = "text/javascript";
+        s.crossOrigin = "anonymous";
+                    s.src = "https://cdn-ssl.vidible.tv/prod/player/js/17.16.1039/vidible-min.js?pid=57f56e7fe3bb564182746711&bcid=5474a5f9e4b05cf9e13a804f&s=true&" + scriptParams;
+
+        s.onload = function () {
+            var options = {
+                pid: "57f56e7fe3bb564182746711",
+                bcid: "5474a5f9e4b05cf9e13a804f",
+                params: p,
+                it: it,
+                site: r,
+                pt: pt,
+                sid: sid,
+                generateGUID: utilsObj.GUID
+            }
+                            options.content = c;
+                                var pl = vidible.createPlayer(options, json, {"cdn":"https://cdn-ssl.vidible.tv/prod/","trk":"https://trk.vidible.tv/trk/","ds":"https://delivery.vidible.tv/","ads":"https://video.adaptv.advertising.com/","adt":"https://video.adaptv.advertising.com/","ptv":"https://portal.vidible.tv/"});
+                                        var i = document.createElement("img");
+                i.src = "https://trk.vidible.tv/trk/js-started.gif?pid=57f56e7fe3bb564182746711&bcid=5474a5f9e4b05cf9e13a804f&s=true&" + pixelParams;
+                        var clb =  window['vidibleInitialize'];
+            if (clb) {
+                if (typeof pl.then === 'function') {
+                    pl.then(clb);
+                } else {
+                    clb(pl);
+                }
+            }
+            s.onload = s.onreadystatechanged = function () {
+            };
+        };
+        s.onreadystatechange = function () {
+            if (s.readyState == 'complete' || s.readyState == 'loaded') {
+                s.onload();
+            }
+        };
+        c.appendChild(s);
+    }
+})();
